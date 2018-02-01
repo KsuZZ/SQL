b6c1fb40abda50869bbda6b0727ff62e2842daf5
KsuZZ <35704204+KsuZZ@users.noreply.github.com>
2018-02-01 21:13:14 +0300

    Add files via upload

---
+++ .gitignore/No.2.sql
@@ -0,0 +1,40 @@
+﻿
+CREATE TABLE preparation 
+(
+name_p VARCHAR(50) PRIMARY KEY, 
+edition VARCHAR(50), 
+accessibility VARCHAR(50), 
+data_edition DATE,  
+expiry_data smallint
+);
+
+CREATE TABLE name_company
+(
+n_company VARCHAR(50) PRIMARY KEY, 
+quality VARCHAR(50),
+data_creature date, 
+n_country VARCHAR(50), 
+address VARCHAR(50), 
+phone integer
+);
+
+CREATE TABLE diseases
+(
+disease_n VARCHAR(50) PRIMARY KEY,
+symptoms VARCHAR(100)
+);
+
+CREATE TABLE medicinal 
+(
+id smallint PRIMARY KEY,
+name_m VARCHAR(50) REFERENCES preparation(name_p), 
+disease VARCHAR(100) REFERENCES diseases(disease_n), 
+country VARCHAR(50), 
+price smallint, 
+company VARCHAR(50) REFERENCES name_company(n_company), 
+rating smallint, 
+dosage VARCHAR(50), 
+comtraindications VARCHAR(50), 
+class_med VARCHAR(50)
+);
+/
---
+++ .gitignore/No.2рус.sql
@@ -0,0 +1,47 @@
+﻿
+CREATE TABLE препараты 
+(
+название_п VARCHAR(50) PRIMARY KEY, 
+форма_выпуска VARCHAR(50), 
+доступность VARCHAR(50), 
+дата_выпуска DATE,  
+срок_годности smallint
+);
+
+CREATE TABLE фирмы
+(
+название_ф VARCHAR(50) PRIMARY KEY, 
+качество VARCHAR(50),
+год_создания date, 
+страна VARCHAR(50), 
+адрес VARCHAR(50), 
+телефон integer
+);
+
+CREATE TABLE заболевания
+(
+название_з VARCHAR(50) PRIMARY KEY,
+симптомы VARCHAR(100)
+);
+
+CREATE TABLE лекарства
+(
+id smallint PRIMARY KEY,
+название VARCHAR(50) REFERENCES препараты(название_п), 
+заболевания_от_чего VARCHAR(100) REFERENCES заболевания(название_з), 
+страна_производитель VARCHAR(50), 
+стоимость smallint, 
+фирма_изготовитель VARCHAR(50) REFERENCES фирмы(название_ф), 
+рейтинг smallint, 
+дозировка VARCHAR(50), 
+противопоказания VARCHAR(50), 
+класс_препарата VARCHAR(50),
+верность boolean
+);
+
+CREATE TABLE лекарства_заболевания
+(
+id_название_з VARCHAR(50) REFERENCES заболевания(название_з),
+id_название VARCHAR(50) REFERENCES препараты(название_п),
+PRIMARY KEY (id_название_з, id_название)
+);
